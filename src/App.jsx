dfe5af40a7cdcdd4489763f5b6ea226c7d2abb83
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-19 19:45:33 +0500

    Second-Updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const App = () => {
+  
+  const collegeData = [
+    {
+      name: 'IET Alwar',
+      city : 'Alwar',
+      website : 'www.ietalwar.ac.in',
+      student : 'Huzaifa'
+    },
+    {
+      name: 'IIT Delhi',
+      city : 'Delhi',
+      website : 'www.iitdelhi.ac.in',
+      student : 'Harsh'
+    },
+    {
+      name: 'DAE Karachi',
+      city : 'Karachi',
+      website : 'www.daekarachi.ac.in',
+      student : 'Ali'
+    }
+  ]
+  
+  
   return (
     <div>
-      
+
       <h1>Nested Looping with components</h1>
 
     </div>
