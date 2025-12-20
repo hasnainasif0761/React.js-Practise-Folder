86cfa23ae2f86aa4195ff6955305f45bbac27e19
hasnainasif0761 <hasnainasif07610761@gmail.com>
2025-12-20 12:51:10 +0500

    Third-Updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,30 @@
-import React from 'react'
-import College from './College'
-
+import React, { useEffect, useState } from 'react'
 const App = () => {
-  
-  const collegeData = [
-    {
-      name: 'IET Alwar',
-      city : 'Alwar',
-      website : 'www.ietalwar.ac.in',
-      student : [
-        {
-          name : 'Naman',
-          age  : 21,
-          email : 'naman@gmail.com'
-        }
-      ]
-    },
-    {
-      name: 'IIT Delhi',
-      city : 'Delhi',
-      website : 'www.iitdelhi.ac.in',
-      student : [
-        {
-          name : 'Rohan',
-          age  : 22,
-          email : 'rohan@gmail.com'
-        }
-      ]
-    },
-    {
-      name: 'DAE Karachi',
-      city : 'Karachi',
-      website : 'www.daekarachi.ac.in',
-      student : [
-        {
-          name : 'Ali',
-          age  : 20,
-          email : 'ali@gmail.com'
-        }
-      ]
+
+    const [counter, setCounter] = useState(0)
+    const [data, setData] = useState(0)
+    
+    function logMessage(message){
+      console.log(message)
     }
-  ]
-  
-  
+    useEffect(()=>{
+      logMessage("useEffect chal rha hai Ake bar");
+    },[])
+    function loginMessage(){
+      console.log("dubara function chal rhi hai")
+    }
+    loginMessage();
+
+
+
+
+
+
   return (
     <div>
-
-      <h1>Nested Looping with components</h1>
-      {
-        collegeData.map((elem,index)=>(
-          <div key={index}>
-          <College college={elem} />
-          </div>
-        ))
-      }
+      <h1>Introduction UseEffect</h1>
+      <button onClick={()=>{setCounter(counter+1)}}>Counter{counter}</button>
+      <button onClick={()=>{setData(data+1)}}>Counter{data}</button>
     </div>
   )
 }
